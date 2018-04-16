929c58c7002819308282433a4ea794855ef3e3af
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-04-17 00:07:06 +0700

    Added prepare directory to data preparation

--- src/data-preparation.py
+++ src/data-preparation.py
@@ -12,6 +12,26 @@ def normalize_image(image, toCanny=False):
     image = cv2.resize(beras, (350, 350))
     return image
 
+def prepare_directory(grades):
+    print('Preparing directory')
+    data_dir = '../data'
+    if not os.path.exists(data_dir):
+        print('Creating data directory')
+        os.makedirs(data_dir)
+
+    for grade in grades:
+        grade_data_dir = data_dir + '/{}'.format(grade)
+        if not os.path.exists(grade_data_dir):
+            print('Creating data/{} directory'.format(grade))
+            os.makedirs(grade_data_dir)
+        else:
+            if os.listdir(grade_data_dir) != []:
+                print('Directory {} is not empty, removing old data')
+                filelist = glob.glob(grade_data_dir + '/*')
+                for file in filelist:
+                    os.remove(file)
+    print("Finished preparing directory")
+
 def process_raw_images(raw_images_path, grades, toCanny=False):
     img_type = 'canny' if toCanny else 'bw'
     print('Processing raw beras data...')
@@ -35,8 +55,26 @@ def process_raw_images(raw_images_path, grades, toCanny=False):
 
 if __name__ == '__main__':
     grades = ['A', 'B', 'C']
-    path = input('Dataset path: ')
 
+    yes = {'yes', 'ye', 'y', ''}
+    no = {'no', 'n'}
+    choice = input('Activate canny mode?[Y/n]').lower()
+
+    while choice not in yes or choice not in no:
+        print('Sorry, did not quite catch that')
+        choice = input('Activate canny mode?[Y/n]').lower()
+
+    canny_mode = False
+    if choice in yes:
+        canny_mode = True
+        print("Canny mode activated")
+    else:
+        print("Black and White mode activated")
+
+    path = input('Dataset path: ')
     if os.path.isdir(path):
-        process_raw_images(path, grades)
+        prepare_directory(grades)
+        process_raw_images(path, grades, canny_mode)
+    else:
+        print('Could not find directory, exiting program')
 
