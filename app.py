fe03d9f7b659d98b4fca9f5140299e1e3c0d6e95
Felix Limanta <13515065@std.stei.itb.ac.id>
2018-05-06 01:09:35 +0700

    Uncommented training code

--- app.py
+++ app.py
@@ -177,20 +177,20 @@ def upload_file():
             }
             return jsonify(reply)
         if file and allowed_file(file.filename):
-            # filename = secure_filename(file.filename) #TODO: Hash filename
-            # file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filename = secure_filename(file.filename) #TODO: Hash filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-            # file.save(file_path)
+            file.save(file_path)
             
-            # #classify_image and return JSON
-            grades = ['A', 'B', 'C']    
-            # image = cv2.imread(file_path)
-            # processed_image = normalize_image(image, True)
+            #classify_image and return JSON
+            grades = ['A', 'B', 'C']
+            image = cv2.imread(file_path)
+            processed_image = normalize_image(image, True)
             
-            # result = predict_image(processed_image, 'canny', grades, 3)
+            result = predict_image(processed_image, 'canny', grades, 3)
             
-            # result_grade = grades[int(result)]
-            result_grade = random.choice(grades)
+            result_grade = grades[int(result)]
+            # result_grade = random.choice(grades)
 
             reply = {
                 "success" : True,
