6b4e2dcdc6920c876db6cbfe3f908dc2036ceea2
Muhamad Bayu Adinegara <39664279+beyangbayu@users.noreply.github.com>
2018-05-30 07:05:01 +0700

    Change bw to canny

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def allowed_file(filename):
 # Method == 2: Grayscale
 # Method == 3: Resize only
 def normalize_image(image, method):
-    image = cv2.resize(image, (500, 500))
+    image = cv2.resize(image, (512, 512))
 
     if (method == 1):
         # Denoising
